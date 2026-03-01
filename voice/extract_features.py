8e052652df98bc808927a58c28db9519a79d3f3e
Heshan Sasiru <116240964+heshan2002@users.noreply.github.com>
2026-03-01 10:17:51 +0530

    remove

--- voice/extract_features.py
+++
@@ -1,7 +0,0 @@
-import librosa
-import numpy as np
-
-def extract_mfcc(file):
-    audio, sr = librosa.load(file, duration=3)
-    mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=40)
-    return np.mean(mfcc.T, axis=0)
